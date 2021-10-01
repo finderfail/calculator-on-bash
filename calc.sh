351c78217b58640a75202ef6c864dd828f368b50
milefxd <91735391+milefxd@users.noreply.github.com>
2021-10-01 15:24:01 +0300

    Update calc.sh

--- calc.sh
+++ calc.sh
@@ -18,5 +18,5 @@ echo "$sum"
 elif [[ ( $1 == "--help" ) ]];
 then
 echo "If says Not perrmissions use chmod +x calc.sh"
-echo "For start working with calculator use than command ./calc.sh 100 20 -"
+echo "For start working with calculator use than command ./calc.sh 100 20 - or bash calc.sh 100 25 -"
 fi
