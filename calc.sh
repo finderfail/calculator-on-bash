b1ae0d470a123e26243b51cfff1e2cd2dd6d83d7
0finderfail <50328441+Shadowmaster346@users.noreply.github.com>
2021-10-01 14:23:05 +0300

    Update calc.sh
    Update 1.0
    Hot fix 1.1

--- calc.sh
+++ calc.sh
@@ -7,6 +7,13 @@ echo "Second number"
 read scd
 echo "You Second number is $scd"
 echo "+-*/"
-# read ndm
+read ndm
+if [[ ( $ndm == "+" ) ]];
+then
 ((sum=$fst+$scd))
 echo "$sum"
+elif [[ ( $ndm == "-" ) ]];
+then
+((sum=$fst-$scd))
+echo "$sum"
+fi
