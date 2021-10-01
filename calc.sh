c8196fad5797d4eb9fffbd18e00ddb1348b18102
milefxd <91735391+milefxd@users.noreply.github.com>
2021-10-01 15:45:21 +0300

    Update calc.sh
    fix

--- calc.sh
+++ calc.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 echo "Small calculator"
 echo "First number"
-read 1
+read $1
 echo "You First number is $1"
 echo "Second number"
-read 2
+read $2
 echo "You Second number is $2"
 echo "+-*/"
-read 3
+read $3
 if [[ ( $3 == "+" ) ]];
 then
 ((sum=$1+$2))
