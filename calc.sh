d087eca21a8436a06f4fa41f9fa3a9c4f2d38f13
0finderfail <50328441+Shadowmaster346@users.noreply.github.com>
2021-10-01 14:49:48 +0300

    Update calc.sh
    calc with arguments 
    ./calc.sh 75 25 +

--- calc.sh
+++ calc.sh
@@ -1,27 +1,31 @@
 #!/bin/bash
-echo "Small calculator"
-echo "First number"
-read fst
-echo "You First number is $fst"
-echo "Second number"
-read scd
-echo "You Second number is $scd"
-echo "+-*/"
-read ndm
-if [[ ( $ndm == "+" ) ]];
+#echo "Small calculator"
+#echo "First number"
+#read fst
+#echo "You First number is $fst"
+#echo "Second number"
+#read scd
+#echo "You Second number is $scd"
+#echo "+-*/"
+#read ndm
+echo "Total arguments : $#"
+echo "1st Argument = $1"
+echo "2nd argument = $2"
+echo "3nd argument = $3"
+if [[ ( $3 == "+" ) ]];
 then
-((sum=$fst+$scd))
+((sum=$1+$2))
 echo "$sum"
-elif [[ ( $ndm == "-" ) ]];
+elif [[ ( $3 == "-" ) ]];
 then
-((sum=$fst-$scd))
+((sum=$1-$2))
 echo "$sum"
-elif [[ ( $ndm == "/" ) ]];
+elif [[ ( $3 == "/" ) ]];
 then
-((sum=$fst/$scd))
+((sum=$1/$2))
 echo "$sum"
-elif [[ ( $ndm == "*" ) ]];
+elif [[ ( $3 == "*" ) ]];
 then
-((sum=$fst*$scd))
+((sum=$1*$2))
 echo "$sum"
 fi
