6ff788d27a6f985fd24bd6092cb7bbf429ecad4e
finderfail <50328441+finderfail@users.noreply.github.com>
2024-05-08 23:04:35 +0300

    Update calc.sh

--- calc.sh
+++ calc.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
-echo "Small calculator"
-echo "Type your numbers"
-read fst
-((fst1=$fst))
-echo "$fst1"
+if [[ ( $1 == "res" ) ]];
+then
+((sum=$2))
+echo "$sum"
+elif [[ ( $1 == "--help" ) ]];
+then
+echo "If says Not perrmissions use chmod +x calc.sh"
+echo "For start working with calculator use than command ./calc.sh res 10+25"
+else
+    echo "Small calculator"
+    echo "Type your numbers or use ./calc.sh res 10+13"
+    read fst
+    ((fst1=$fst))
+    echo "$fst1"
+fi
