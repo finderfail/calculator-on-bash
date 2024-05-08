f3815e51abc17fbe3759f77a1f0537acbabbed1c
finderfail <50328441+finderfail@users.noreply.github.com>
2024-05-08 22:59:52 +0300

    Update calc.sh

--- calc.sh
+++ calc.sh
@@ -1,27 +1,6 @@
 #!/bin/bash
 echo "Small calculator"
-echo "Type your first number"
+echo "Type your numbers"
 read fst
-echo "You First number is $fst"
-echo "Type your second number"
-read scd
-echo "You Second number is $scd"
-echo "Choose an operator: +-*/"
-read ndm
-if [[ ( $ndm == "+" ) ]];
-then
-((sum=$fst+$scd))
-echo "$sum"
-elif [[ ( $ndm == "-" ) ]];
-then
-((sum=$fst-$scd))
-echo "$sum"
-elif [[ ( $ndm == "/" ) ]];
-then
-((sum=$fst/$scd))
-echo "$sum"
-elif [[ ( $ndm == "*" ) ]];
-then
-((sum=$fst*$scd))
-echo "$sum"
-fi
+((fst1=$fst))
+echo "$fst1"
